63f54560a4087d890549d97c5f1ac75922d0c3fc
Cem Turan <cemx0121@edu.zealand.dk>
2022-09-05 13:26:05 +0200

    Tilføjet studerende klasse

---
+++ ClassRoomNet60/Studerende.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoomNet60
+{
+    public class Studerende
+    {
+        public string Navn { get; private set; }
+        public int Fødselsmåned { get; private set; }
+
+        public int Fødselsdag { get; private set; }
+
+        public Studerende(string navn, int fødselsmåned, int fødselsdag)
+        {
+            Navn = navn;
+            Fødselsmåned = fødselsmåned;
+            Fødselsdag = fødselsdag;
+        }
+    }
+}
